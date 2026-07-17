fadc24357e75fe14ad06611549786a74f01266bc
kossi yayra <afanvi.michel@gmail.com>
2026-07-17 20:39:07 -0300

    Primeiro exercicio

---
+++ src/ListaEx/Exo1.java
@@ -0,0 +1,12 @@
+package ListaEx;
+
+public class Exo1 {
+    static void monstrarNome(String nome){
+        System.out.println("Òla "+nome+"!  Bem-vindo ao SENAI");
+    }
+    public static void main(String[] args) {
+        monstrarNome("kossi");
+        monstrarNome("yayra");
+        monstrarNome("mikelvski");
+    }
+}
