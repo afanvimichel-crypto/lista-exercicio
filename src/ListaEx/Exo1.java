f9beb50671ebe6b924e1528e3d55b57e98b529fe
kossi yayra <afanvi.michel@gmail.com>
2026-07-17 20:56:13 -0300

    exo1 certo

--- src/ListaEx/Exo1.java
+++ src/ListaEx/Exo1.java
@@ -1,12 +1,12 @@
 package ListaEx;
 
 public class Exo1 {
-    static void monstrarNome(String nome){
+    static void exibirSaudacao(String nome){
         System.out.println("Òla "+nome+"!  Bem-vindo ao SENAI");
     }
     public static void main(String[] args) {
-        monstrarNome("kossi");
-        monstrarNome("yayra");
-        monstrarNome("mikelvski");
+        exibirSaudacao("kossi");
+        exibirSaudacao("yayra");
+        exibirSaudacao("mikelvski");
     }
 }
