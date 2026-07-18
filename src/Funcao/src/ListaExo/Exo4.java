38fea165467e5dbbee4f3900f9790500ed02154f
kossi yayra afanvi <afanvi.michel@gmail.com>
2026-07-18 12:08:45 -0300

    exo 4 certo

---
+++ src/Funcao/src/ListaExo/Exo4.java
@@ -0,0 +1,23 @@
+package ListaExo;
+
+import java.util.Scanner;
+
+public class Exo4 {
+   static void analisarSenha(String senha){
+        if(senha.length()<6){
+            System.out.println("SENHA FRACA");
+        } else if (senha.length()<9) {
+            System.out.println("SENHA MEDIA");
+
+        }else {
+            System.out.println("SENHA FORTE");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Informe sua sanha:");
+        String mPass=sc.nextLine();
+        analisarSenha(mPass);
+
+    }
+}
